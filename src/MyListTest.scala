72bcad35714b4539b4737cccdcbb69f0b6c92ff3
Farzan <f.behzadian@digikala.com>
2021-03-31 13:32:52 +0430

    Progress in chapter 3

--- src/MyListTest.scala
+++ src/MyListTest.scala
@@ -46,5 +46,67 @@ object MyListTest {
     println(List.append(List(1, 2, 3), List(7, 8, 9)))
     println(List.init(List(1, 2, 3, 4)))
     println(List.dropWhile2(List(1, 2, 3, 4, 5))(n => n < 4))
+
+    println("7===")
+    println(List.sum2(List(1, 2, 3, 4)))
+    println(List.product2(List(1, 2, 3, 4)))
+
+    println("8===")
+    println(List.foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
+    println(List.foldRight(List(1.0, 2, 3), Nil: List[Double])(Cons(_, _)))
+    println(List.length(List(1, 2, 3, 4, 5)))
+    println(List.length2(List(1, 2, 3, 4, 5)))
+
+    println("9===")
+    println(List.foldRight(List(1, 2, 3, 4), 1.0)(_ * _))
+    println(List.foldLeft(List(1, 2, 3, 4), 1.0)(_ * _))
+    println(List.foldLeft(List(1, 2, 3, 4), 0)(_ + _))
+
+    println("10===")
+    println(List.reverse(List(1, 2, 3)))
+    println(List.reverse(List(1)))
+    println(List.reverse(Nil))
+
+    println(List.reverse2(List(1, 2, 3)))
+    println(List.reverse2(List(1)))
+    println(List.reverse2(Nil))
+
+    println("11===")
+    println(List.append2(List(1, 2, 3), List(7, 8, 9)))
+    println(List.append3(List(1, 2, 3), List(7, 8, 9)))
+    println(List.concat(List(List(1, 2), List(3, 4), List(5, 6))))
+
+    println("3.16 ==")
+    println(List.addOne(List(1, 2, 3)))
+    println(List.addOne(Nil))
+    println(List.addOne2(List(1, 2, 3)))
+    println(List.addOne2(Nil))
+
+    println("3.17 ==")
+    println(List.doubleToString(List(1, 2, 3)))
+
+    println("3.18 ==")
+    println(List.map(List(1, 2, 3))(a => a + 1))
+
+    println("3.19 ==")
+    println(List.filter(List(1, 2, 3))(a => a != 2))
+    println(List.filter(List(1, 2, 3))(a => a == 4))
+
+    println("3.20 ==")
+    println(List.flatMap(List(1,2,3))(i => List(i,i)))
+
+    println("3.21 ==")
+    println(List.filterViaFlatMap(List(1, 2, 3))(a => a != 2))
+    println(List.filterViaFlatMap(List(1, 2, 3))(a => a == 4))
+
+    println("=== 3.22")
+    println(List.addPairwise(List(1, 2, 3), List(4, 5, 6)))
+    println(List.addPairwise(List(1, 2), List(4, 5, 6)))
+    println(List.addPairwise(List(1, 2, 3), List(4, 5)))
+
+    println("=== 3.22")
+    println(List.zipWith(List(1, 2, 3), List(1.1, 2.2, 3.3))((a, b) => String.format("{%d, %f}", a, b)))
+
+
   }
 }
--- src/farzan/hello/list/List.scala
+++ src/farzan/hello/list/List.scala
@@ -68,4 +68,107 @@ object List {
       case Cons(h, t) if f(h) => dropWhile2(t)(f)
       case _ => as
     }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(as: List[Int]): Int =
+    foldRight(as, 0)((x, y) => x + y)
+
+  def product2(as: List[Double]): Double =
+    foldRight(as, 1.0)(_ * _)
+
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, acc) => acc + 1)
+
+  def length2[A](as: List[A]): Int = {
+    as match {
+      case Nil => 0
+      case Cons(_, t) => length2(t) + 1
+    }
+  }
+
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  def reverse[A](as: List[A]): List[A] = {
+    @tailrec
+    def loop(xs: List[A], acc: List[A]): List[A] = {
+      xs match {
+        case Nil => Nil
+        case Cons(h, Nil) => Cons(h, acc)
+        case Cons(h, t) => loop(t, Cons(h, acc))
+      }
+    }
+    loop(as, List[A]())
+  }
+
+  def reverse2[A](as: List[A]): List[A] =
+    foldLeft(as, List[A]())((acc, a) => Cons(a, acc))
+
+  // Exercise 3.13; I couldn't solve it. I event could not understand the solution :-D
+//  def foldRightViaFoldLeft[A, B](as: List[A], z: B)(f:(A, B) => B): B =
+
+  def append2[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, a2)((a, b) => Cons(a, b))
+
+  def append3[A](a1: List[A], a2: List[A]): List[A] =
+    foldLeft(a1, a2)((b, a) => Cons(a, b))
+
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, List[A]())(append)
+
+  def addOne(l: List[Int]): List[Int] =
+    l match {
+      case Nil => Nil
+      case Cons(h, Nil) => Cons(h + 1, Nil)
+      case Cons(h, t) => Cons(h + 1, addOne(t))
+    }
+
+  def addOne2(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((h, t) => Cons(h + 1, t))
+
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((h, t) => Cons(h.toString.+('d'), t))
+
+  def map[A, B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRight(l, Nil: List[A])(
+      (h, t) =>
+        if (f(h)) Cons(h, t)
+        else t
+    )
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+    concat(map(l)(f))
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a =>
+      if (f(a)) List(a)
+      else Nil
+    )
+
+  def addPairwise(l1: List[Int], l2: List[Int]): List[Int] =
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+    }
+
+  def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] =
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
+
 }
